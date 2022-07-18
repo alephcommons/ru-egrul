0ea5ce812d76d54526bab47dc432fe309bb7e8d7
Artem Chernov <artemlime.chernov@gmail.com>
2022-07-19 01:29:42 +0300

    Refactored: moved all csv processing from entity code.

--- rusregistry/entities/orgchief.py
+++ rusregistry/entities/orgchief.py
@@ -1,24 +1,17 @@
-from entities.entity import ConnectionEntity
+from entities.entity import Entity
 import pandas as pd
 from csv_processing.utils import fix_inn
 
-class OrgChief(ConnectionEntity):
+class OrgChief(Entity):
     def __init__(self, item):
         super().__init__(item)
         self.entity_type = 'Directorship'
 
     def from_csv_row(self, row: pd.core.series.Series):
-        row['inn'] = fix_inn(row['inn'])
-        row['chief_inn'] = fix_inn(row['chief_inn'])
-
-        organisation_id = self.add_id_prefix(row['ogrn'], 'ogrn')
-        director_id = self.add_id_prefix(row['chief_inn'], 'inn')
-
-        row = self.fix_date(row)
-        self.data_dict = {'director': director_id,
-                          'organization': organisation_id,
+        self.data_dict = {'director': row['director_id'],
+                          'organization': row['organization_id'],
                           'role': row['chief_position'],
-                          'startDate': row['cdate_num']}
+                          'startDate': row['startDate']}
 
     def make_id(self, entity):
         entity.id = f"{self.data_dict['director']}-orgchief-{self.data_dict['organization']}"
